41dd26dfdf8ca739dd4aa6d93f7931da073fc3cc
franck <carnet.franck.paul@gmail.com>
2012-11-05 18:00:44 +0100

    Update locales

--- _define.php
+++ _define.php
@@ -12,8 +12,8 @@
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
-	/* Name */			"sysInfo",
-	/* Description*/		"System Information",
+	/* Name */				__("sysInfo"),
+	/* Description*/		__("System Information"),
 	/* Author */			"Franck Paul",
 	/* Version */			'0.7'
 );
