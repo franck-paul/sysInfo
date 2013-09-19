2a18215aa2fc63bb58e65217a457949135bbb0b3
franck <carnet.franck.paul@gmail.com>
2013-09-19 13:25:08 +0200

    various

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-# -- BEGIN LICENSE BLOCK ---------------------------------------
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of sysInfo, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) Franck Paul and contributors
+# carnet.franck.paul@gmail.com
 #
-# Copyright (c) Franck Paul
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
-# -- END LICENSE BLOCK -----------------------------------------
+# -- END LICENSE BLOCK ------------------------------------
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
