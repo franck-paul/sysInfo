6ec41fb041e9f395c04cb97229f770cb3799b951
franck <carnet.franck.paul@gmail.com>
2013-09-19 08:11:18 +0200

    locales

--- _admin.php
+++ _admin.php
@@ -9,7 +9,11 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 # -- END LICENSE BLOCK -----------------------------------------
+
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
+
+/* Name */			__('sysInfo');
+/* Description*/	__('System Information');
 
 $_menu['System']->addItem(__('System info'),'plugin.php?p=sysInfo','index.php?pf=sysInfo/icon.png',
 		preg_match('/plugin.php\?p=sysInfo(&.*)?$/',$_SERVER['REQUEST_URI']),
--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 #
 # This file is part of Dotclear 2.
 #
-# Copyright (c) 2003-2011 Franck Paul
+# Copyright (c) Franck Paul
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 # -- END LICENSE BLOCK -----------------------------------------
+
+if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
 $checklists = array(
 	__('Compiled templates') => 'templates',
