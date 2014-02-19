1b785fd1140f2ee2ccec2f53259f5dd211e31887
franck <carnet.franck.paul@gmail.com>
2014-02-19 18:57:11 +0100

    New template set currywurst (HTML5)

--- _public.php
+++ _public.php
@@ -44,7 +44,12 @@ class urlSysInfo extends dcUrlHandlers
 
 		if ($args == 'behaviours') {
 
-			$core->tpl->setPath($core->tpl->getPath(), dirname(__FILE__).'/default-templates');
+			$tplset = $core->themes->moduleInfo($core->blog->settings->system->theme,'tplset');
+			if (!empty($tplset) && is_dir(dirname(__FILE__).'/default-templates/'.$tplset)) {
+				$core->tpl->setPath($core->tpl->getPath(), dirname(__FILE__).'/default-templates/'.$tplset);
+			} else {
+				$core->tpl->setPath($core->tpl->getPath(), dirname(__FILE__).'/default-templates/'.DC_DEFAULT_TPLSET);
+			}
 			self::serveDocument('behaviours.html');
 			exit;
 
