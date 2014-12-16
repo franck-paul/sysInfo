dc87050be0c8bcaaa9071a533e3a9e89166d31fd
franck <carnet.franck.paul@gmail.com>
2014-12-16 14:33:08 +0100

    Adapt admin URL for 2.7.1, next release will be 1.1

--- _define.php
+++ _define.php
@@ -16,7 +16,7 @@ $this->registerModule(
 	/* Name */			"sysInfo",
 	/* Description*/		"System Information",
 	/* Author */			"Franck Paul",
-	/* Version */			'1.0',
+	/* Version */			'1.1',
 	array(
 		/* Type */			'type' =>			'plugin'
 	)
