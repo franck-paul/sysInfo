c0aea239e1cc63b10cba03374b8a9c997ef855f3
franck <carnet.franck.paul@gmail.com>
2023-05-15 11:12:54 +0200

    Release 4.5

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'sysInfo',
     'System Information',
     'Franck Paul',
-    '4.4',
+    '4.5',
     [
         'requires' => [['core', '2.26']],
         'type'     => 'plugin',
