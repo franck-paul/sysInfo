12fbff3edb340e2a119cb73633420c734b8b2d0b
franck <carnet.franck.paul@gmail.com>
2024-01-09 12:45:32 +0100

    New release 8.15

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'sysInfo',
     'System Information',
     'Franck Paul',
-    '8.14',
+    '8.15',
     [
         'requires' => [['core', '2.28']],
         'type'     => 'plugin',
