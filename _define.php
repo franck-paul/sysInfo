ced1bfe3ca0645edba2b6f257e858d3729ad5822
franck <carnet.franck.paul@gmail.com>
2025-01-08 18:10:11 +0100

    New release 10.6

--- _define.php
+++ _define.php
@@ -15,7 +15,7 @@ $this->registerModule(
     'System Information',
     'System Information',
     'Franck Paul',
-    '10.5',
+    '10.6',
     [
         'requires' => [['core', '2.33']],
         'type'     => 'plugin',
