e0df5eb0c3272b2c2d87ae504ec481c0fcfbd0ce
franck <carnet.franck.paul@gmail.com>
2025-03-13 17:24:12 +0100

    New release 11.3

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'System Information',
     'System Information',
     'Franck Paul',
-    '11.2',
+    '11.3',
     [
-        'date'     => '2025-03-13T12:24:56+0100',
+        'date'     => '2025-03-13T17:24:12+0100',
         'requires' => [['core', '2.34']],
         'type'     => 'plugin',
         'priority' => 99_999_999_999,
