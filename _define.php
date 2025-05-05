b370cb34ff85a9ee23bfd2b2b1dc363e74190474
franck <carnet.franck.paul@gmail.com>
2025-05-05 13:05:48 +0200

    New release 12.5

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'System Information',
     'System Information',
     'Franck Paul',
-    '12.4',
+    '12.5',
     [
-        'date'     => '2025-05-05T09:44:56+0200',
+        'date'     => '2025-05-05T13:05:48+0200',
         'requires' => [
             ['core', '2.34'],
             ['TemplateHelper'],
