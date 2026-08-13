81ca87b43392cd7936ce295a5b473b6469836ec1
franck <contact@open-time.net>
2026-08-13 16:28:13 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,27 +11,31 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'System Information',
-    'System Information',
-    'Franck Paul',
-    '16.0',
-    [
-        'date'     => '2026-08-03T10:12:24+0200',
-        'requires' => [
-            ['core', '2.39'],
-            ['TemplateHelper'],
-        ],
-        'type'     => 'plugin',
-        'priority' => 99_999_999_999,
+declare(strict_types=1);
 
-        'settings' => [
-            'blog' => '#params.sysinfo',
-        ],
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'System Information',
+        'System Information',
+        'Franck Paul',
+        '16.0',
+        [
+            'date'     => '2026-08-03T10:12:24+0200',
+            'requires' => [
+                ['core', '2.39'],
+                ['TemplateHelper'],
+            ],
+            'type'     => 'plugin',
+            'priority' => 99_999_999_999,
 
-        'details'    => 'https://open-time.net/?q=sysinfo',
-        'support'    => 'https://github.com/franck-paul/sysInfo',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/sysInfo/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+            'settings' => [
+                'blog' => '#params.sysinfo',
+            ],
+
+            'details'    => 'https://open-time.net/?q=sysinfo',
+            'support'    => 'https://github.com/franck-paul/sysInfo',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/sysInfo/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
